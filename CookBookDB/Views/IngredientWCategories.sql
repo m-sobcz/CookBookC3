729339090bb4bfc45ac31ed1ca14bf08359b8a03
MSobczak <m.sobcz@gmail.com>
2020-06-29 23:37:12 +0200

    DB project organization

---
+++ CookBookDB/Views/IngredientWCategories.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[IngredientWCategories]
+	AS 
+	SELECT i.* FROM Ingredients i
+	Left join Ingredients_Categories ic on i.id=ic.Ingredients_Id
+	Left join Categories c on c.id=ic.Categories_Id
